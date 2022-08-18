3ee8751987eafd0aaacba921dd74de840b04ae7f
Baizho <bossnurmyrza@gmail.com>
2022-08-19 01:39:37 +0600

    Update index.js

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from 'next/Image';
+import Image from 'next/image';
 
 function Card(props) {
   const [likes, setlikes] = useState(0);
